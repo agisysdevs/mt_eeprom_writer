487dc5aadc30bd997eeba5017f34a99cb770363c
pablacho <pablacho@gmail.com>
2023-11-24 16:44:12 -0800

    version 0.1 release, everything working on tests

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mt_eeprom_writer
 ESP32 program to write serial code received from UART on eeprom
+
+Memory writing protocol
+1) using a terminal, type the text to be saved according to SCM_STD_003_Marking_Standard_for_MT_Designed_Electronic_Assemblies.docx Version <1.3>, November 2022, item 6
+example: 
+PN 1 "00205675" CR 2 "A" PY 1 "08" PW 1 "25" SN 2 "0000000000620089" MI 1 "S" RI 1 "R" MAC 1 "00:10:52:D2:43:FB"
+send <CR><LF> to confirm and send the text to the microcontroller
+2) possible answers:
+2.1) E0: buffer overflow, it has more than 256 characters. Prints the sent buffer.
+2.2) E1: buffer too short, it has less than 100 chars. Prints the sent buffer.
+2.3) E2: memory read error. Prints the error given by the processor. 
+2.4) E3: memory not empty. Prints the information saved on the target address.
+2.5) E4: memory write error. Prints the error given by the processor. 
+2.6) E5: memory read error. Prints the error given by the processor.
+2.7) E6: comparison error, the read string is different than the one written. Prints the read buffer. 
+2.8): OK: everyting went OK, prints the read buffer. 
+3) commands
+3.1) ff: erases the memory (writes 0xFF on every slot), sends "ME OK" when finished
+3.2) vv: prints software version and date, example of current version:
+V: 0.1 D: 20231124
+3.3) other commands, for debugging purposes:
+3.3.1) mf: memory fill, sets the program to fill the memory with the next sent character
+3.3.2) mp: prints the full extent of the memory
+3.3.3) dp: detects and print all the devices on the i2c bus
--- main/mt_eeprom_writer.c
+++ main/mt_eeprom_writer.c
@@ -14,13 +14,17 @@
 #define I2C_MASTER_NUM I2C_NUM_0
 #define I2C_SDA_PIN 21
 #define I2C_SCL_PIN 22
-#define EEPROM_ADDR 0x52       // 0x50
-#define EEPROM_START_ADDR 0x00 // 0x200
+#define EEPROM_ADDR 0x50        // 0x52
+#define EEPROM_START_ADDR 0x200 // 0x00
 #define EEPROM_PAGE_SIZE 16
+#define EEPROM_SIZE 0x4000 // 0x100
 #define MAX_TEXT_LENGTH 256
 #define MIN_TEXT_LENGTH 10 // 100
 #define MAIN_PROCESSOR_RESET_PIN (GPIO_NUM_13)
 #define STATUS_LED (GPIO_NUM_16)
+
+#define SOFTWARE_VERSION    "0.1"
+#define SOFTWARE_DATE       "20231124"
 
 void init_uart()
 {
@@ -46,18 +50,25 @@ void init_i2c()
     i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
 }
 
-esp_err_t eeprom_write(uint8_t deviceaddress, uint16_t eeaddress, uint8_t *data, size_t size)
+esp_err_t eeprom_write(uint8_t deviceaddress, uint16_t eeaddress, uint8_t *data, uint16_t size)
 {
     esp_err_t ret = ESP_OK;
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    // char debug[200];
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (deviceaddress << 1) | I2C_MASTER_WRITE, 1);
-    // i2c_master_write_byte(cmd, eeaddress >> 8, 1);
+    if (EEPROM_SIZE > 0x100)
+        i2c_master_write_byte(cmd, (eeaddress >> 8) & 0xFF, 1);
     i2c_master_write_byte(cmd, eeaddress & 0xFF, 1);
 
-    int bytes_remaining = size;
-    int current_address = eeaddress;
-    int first_write_size = ((EEPROM_PAGE_SIZE - 1) - eeaddress % (EEPROM_PAGE_SIZE - 1)) + 1;
+    uint16_t bytes_remaining = size;
+    uint16_t current_address = eeaddress;
+    uint16_t aux_size_0 = eeaddress % EEPROM_PAGE_SIZE;
+    uint16_t first_write_size = EEPROM_PAGE_SIZE - aux_size_0;
+
+    // sprintf(debug, "bytes_rem: %d, curr_add: %.4x, first write size: %d\n", bytes_remaining, current_address, first_write_size);
+    // uart_write_bytes(UART_NUM, debug, strlen(debug));
+
     if (eeaddress % (EEPROM_PAGE_SIZE - 1) == 0 && eeaddress != 0)
         first_write_size = 1;
     if (bytes_remaining <= first_write_size)
@@ -76,15 +87,18 @@ esp_err_t eeprom_write(uint8_t deviceaddress, uint16_t eeaddress, uint8_t *data,
             return ret;
         while (bytes_remaining > 0)
         {
+            // sprintf(debug, "bytes_rem: %d, curr_add: %.4x\n", bytes_remaining, current_address);
+            // uart_write_bytes(UART_NUM, debug, strlen(debug));
             cmd = i2c_cmd_link_create();
 
-            // 2ms delay period to allow EEPROM to write the page
+            // 5ms delay period to allow EEPROM to write the page
             // buffer to memory.
-            vTaskDelay(20 / portTICK_PERIOD_MS);
+            vTaskDelay(50 / portTICK_PERIOD_MS);
 
             i2c_master_start(cmd);
             i2c_master_write_byte(cmd, (deviceaddress << 1) | I2C_MASTER_WRITE, 1);
-            // i2c_master_write_byte(cmd, current_address >> 8, 1);
+            if (EEPROM_SIZE > 0x100)
+                i2c_master_write_byte(cmd, (current_address >> 8) & 0xFF, 1);
             i2c_master_write_byte(cmd, current_address & 0xFF, 1);
             if (bytes_remaining <= EEPROM_PAGE_SIZE)
             {
@@ -113,7 +127,8 @@ esp_err_t eeprom_read(uint8_t deviceaddress, uint16_t eeaddress, uint8_t *data, 
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (deviceaddress << 1) | I2C_MASTER_WRITE, 1);
-    // i2c_master_write_byte(cmd, eeaddress << 8, 1);
+    if (EEPROM_SIZE > 0x100)
+        i2c_master_write_byte(cmd, (eeaddress >> 8) & 0xFF, 1);
     i2c_master_write_byte(cmd, eeaddress & 0xFF, 1);
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (deviceaddress << 1) | I2C_MASTER_READ, 1);
@@ -131,23 +146,53 @@ esp_err_t eeprom_read(uint8_t deviceaddress, uint16_t eeaddress, uint8_t *data, 
 
 void fill_mem(uint8_t value)
 {
-    uint8_t buffer[MAX_TEXT_LENGTH];
-    memset(buffer, value, MAX_TEXT_LENGTH);
-    // write_to_eeprom(buffer, 0x00, MAX_TEXT_LENGTH);
-    eeprom_write(EEPROM_ADDR, 0x00, buffer, MAX_TEXT_LENGTH);
+    uint8_t buffer[EEPROM_SIZE];
+    memset(buffer, value, EEPROM_SIZE);
+    eeprom_write(EEPROM_ADDR, 0x00, buffer, EEPROM_SIZE);
 }
 
 void read_mem(void)
 {
-    char memdump[0x100], mdp[7];
+    char memdump[EEPROM_SIZE], mdp[30];
     // read_from_eeprom((uint8_t *)memdump, 0x00, 0xFF);
-    eeprom_read(EEPROM_ADDR, 0x00, (uint8_t *)memdump, MAX_TEXT_LENGTH);
-    for (uint16_t i = 0; i < 0x100; i++)
+    eeprom_read(EEPROM_ADDR, 0x00, (uint8_t *)memdump, EEPROM_SIZE);
+    for (uint16_t i = 0; i < EEPROM_SIZE; i++)
     {
         if (i % 16 == 0)
             uart_write_bytes(UART_NUM, "\n", 1);
-        sprintf(mdp, "%02x: %c ", i, memdump[i]);
-        uart_write_bytes(UART_NUM, mdp, 7);
+        sprintf(mdp, "%04x: %c ", i, memdump[i]);
+        uart_write_bytes(UART_NUM, mdp, 9);
+    }
+    uart_write_bytes(UART_NUM, "\n", 1);
+}
+
+void detect_devices(void)
+{
+    esp_err_t res;
+    char aux[20];
+    uart_write_bytes(UART_NUM, "       0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n", 54);
+    uart_write_bytes(UART_NUM, "0000:         ", 14);
+    for (uint8_t i = 3; i < 0x78; i++)
+    {
+        i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1 /* expect ack */);
+        i2c_master_stop(cmd);
+
+        res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
+        if (i % 16 == 0)
+        {
+            sprintf(aux, "\n%.4x:", i);
+            uart_write_bytes(UART_NUM, aux, 6);
+        }
+        if (res == 0)
+        {
+            sprintf(aux, " %.2x", i);
+            uart_write_bytes(UART_NUM, aux, 3);
+        }
+        else
+            uart_write_bytes(UART_NUM, " --", 3);
+        i2c_cmd_link_delete(cmd);
     }
     uart_write_bytes(UART_NUM, "\n", 1);
 }
@@ -158,40 +203,10 @@ void uart_task(void *pvParameters)
     char empty[MAX_TEXT_LENGTH + 1];  // to check if memory is empty
     char aux0[MAX_TEXT_LENGTH + 1];   // to check if memory is empty
     char aux1[MAX_TEXT_LENGTH + 1];   // to compare read/write results
-    char ans[MAX_TEXT_LENGTH + 10];   // to send back the written result
+    char aux2[MAX_TEXT_LENGTH + 1];   // to compare read/write results
+    char ans[MAX_TEXT_LENGTH + 20];   // to send back the written result
     uint16_t index = 0;
     bool memfill = false;
-
-    memset(empty, 0, MAX_TEXT_LENGTH + 1);
-
-    // while (1)
-    // {
-    //     esp_err_t res;
-    //     printf("     0  1  2  3  4  5  6  7  8  9  a  b  c  d  e  f\n");
-    //     printf("00:         ");
-    //     for (uint8_t i = 3; i < 0x78; i++)
-    //     {
-    //         i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-    //         i2c_master_start(cmd);
-    //         i2c_master_write_byte(cmd, (i << 1) | I2C_MASTER_WRITE, 1 /* expect ack */);
-    //         i2c_master_stop(cmd);
-
-    //         res = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 10 / portTICK_PERIOD_MS);
-    //         if (i % 16 == 0)
-    //             printf("\n%.2x:", i);
-    //         if (res == 0)
-    //             printf(" %.2x", i);
-    //         else
-    //             printf(" --");
-    //         i2c_cmd_link_delete(cmd);
-    //     }
-    //     printf("\n");
-    //     vTaskDelay(pdMS_TO_TICKS(1000));
-    // }
-
-    // write_to_eeprom((uint8_t *)"zzzzzzzzzzzzzzzz", 0x20, 16);
-
-    // read_mem();
 
     while (1)
     {
@@ -202,6 +217,11 @@ void uart_task(void *pvParameters)
         {
             if (data[0] == '\r')
             {
+
+                vTaskDelay(250 / portTICK_PERIOD_MS);
+                gpio_set_level(MAIN_PROCESSOR_RESET_PIN, 1);
+                gpio_set_level(STATUS_LED, 1);
+
                 if (memcmp(buffer, "mf", 2) == 0)
                 {
                     memfill = true;
@@ -214,9 +234,23 @@ void uart_task(void *pvParameters)
                     }
                     memfill = false;
                 }
+                else if (memcmp(buffer, "ff", 2) == 0)
+                {
+                    fill_mem(0xFF);
+                    uart_write_bytes(UART_NUM, "ME OK\n", 6); // Memory erase OK
+                }
                 else if (memcmp(buffer, "mp", 2) == 0)
                 {
                     read_mem();
+                }
+                else if (memcmp(buffer, "dp", 2) == 0)
+                {
+                    detect_devices();
+                }
+                else if (memcmp(buffer, "vv", 2) == 0)
+                {
+                    sprintf(ans, "V: %s D: %s\n", SOFTWARE_VERSION, SOFTWARE_DATE);
+                    uart_write_bytes(UART_NUM, ans, strlen(ans)); // print version and date
                 }
                 else
                 {
@@ -226,70 +260,80 @@ void uart_task(void *pvParameters)
 
                     if (index >= MIN_TEXT_LENGTH)
                     {
-                        esp_err_t read_result = eeprom_read(EEPROM_ADDR, EEPROM_START_ADDR, (uint8_t *)aux0, index);
+                        esp_err_t read_result = eeprom_read(EEPROM_ADDR, EEPROM_START_ADDR, (uint8_t *)aux0, index + 1);
                         if (read_result == ESP_OK)
                         {
-                            // if (strcmp(empty, aux0) == 0)
-                            // {
-                            if (strcmp(empty, aux0) != 0)
+                            memset(empty, 0xFF, index);
+                            empty[index] = '\0';
+                            if (memcmp(empty, aux0, index) == 0)
                             {
-                                aux0[index] = '\0';             // terminate aux0 no matter what just in case i am writing a string smaller than the one recorded
-                                sprintf(ans, "E3: %s\n", aux0); // E3: memory not empty
-                                uart_write_bytes(UART_NUM, ans, strlen(aux0) + 5);
-                            }
+                                // if (memcmp(empty, aux0, index) != 0)
+                                // {
+                                //     aux0[index] = '\0';             // terminate aux0 no matter what just in case i am writing a string smaller than the one recorded
+                                //     sprintf(ans, "E3: %s\n", aux0); // E3: memory not empty
+                                //     uart_write_bytes(UART_NUM, ans, strlen(ans));
+                                //     // for (uint16_t i = 0; i < index; i++) {
+                                //     //     sprintf(ans, "%.4d: %.2x\n", i, aux0[i]);
+                                //     //     uart_write_bytes(UART_NUM, ans, strlen(ans));
+                                //     // }
+                                // }
 
-                            // sprintf(ans, "O3: %s\n", buffer); // output 0
-                            // uart_write_bytes(UART_NUM, ans, index + 5);
-                            esp_err_t write_result = eeprom_write(EEPROM_ADDR, EEPROM_START_ADDR, (uint8_t *)buffer, index);
-
-                            if (write_result == ESP_OK)
-                            {
-                                read_result = eeprom_read(EEPROM_ADDR, EEPROM_START_ADDR, (uint8_t *)buffer, index);
-                                buffer[index] = '\0';
-                                if (read_result == ESP_OK)
+                                esp_err_t write_result = eeprom_write(EEPROM_ADDR, EEPROM_START_ADDR, (uint8_t *)buffer, index + 1);
+                                if (write_result == ESP_OK)
                                 {
-                                    if (strcmp(buffer, aux1) == 0)
+                                    vTaskDelay(100 / portTICK_PERIOD_MS);
+                                    esp_err_t second_read_result = eeprom_read(EEPROM_ADDR, EEPROM_START_ADDR, (uint8_t *)aux2, index + 1);
+                                    // aux2[index] = '\0';
+                                    if (second_read_result == ESP_OK)
                                     {
-                                        sprintf(ans, "OK: %s\n", buffer); // OK
-                                        uart_write_bytes(UART_NUM, ans, index + 5);
+                                        if (strcmp(aux2, aux1) == 0)
+                                        {
+                                            sprintf(ans, "OK: %s\n", aux2); // OK
+                                            uart_write_bytes(UART_NUM, ans, strlen(ans));
+                                        }
+                                        else
+                                        {
+                                            sprintf(ans, "E6: %s\n", aux2); // E6: written / read values do not match
+                                            uart_write_bytes(UART_NUM, ans, strlen(ans));
+                                        }
                                     }
                                     else
                                     {
-                                        sprintf(ans, "E6: %s\n", buffer); // E6: written / read values do not match
-                                        uart_write_bytes(UART_NUM, ans, index + 5);
+                                        sprintf(ans, "E5: %02d - %s\n", second_read_result, aux2); // E5: re-read memory error
+                                        uart_write_bytes(UART_NUM, ans, strlen(ans));
                                     }
                                 }
                                 else
                                 {
-                                    sprintf(ans, "E5: %02d\n", read_result); // E5: re-read memory error
-                                    uart_write_bytes(UART_NUM, ans, 8);
+                                    sprintf(ans, "E4: %02d\n", write_result); // E4: write memory error
+                                    uart_write_bytes(UART_NUM, ans, strlen(ans));
                                 }
                             }
                             else
                             {
-                                uart_write_bytes(UART_NUM, "E4\n", 3); // E4: write memory error
+                                aux0[index] = '\0';             // terminate aux0 no matter what just in case i am writing a string smaller than the one recorded
+                                sprintf(ans, "E3: %s\n", aux0); // E3: memory not empty
+                                uart_write_bytes(UART_NUM, ans, strlen(ans));
                             }
-                            // }
-                            // else
-                            // {
-                            //     sprintf(ans, "E3: %s\n", aux0); // E3: memory not empty
-                            //     uart_write_bytes(UART_NUM, ans, index + 5);
-                            // }
                         }
                         else
                         {
                             sprintf(ans, "E2: %02d\n", read_result); // E2: read memory error
-                            uart_write_bytes(UART_NUM, ans, 8);
+                            uart_write_bytes(UART_NUM, ans, strlen(ans));
                         }
                     }
                     else
                     {
                         sprintf(ans, "E1: %s\n", buffer); // E1: buffer too short
-                        uart_write_bytes(UART_NUM, ans, index + 5);
+                        uart_write_bytes(UART_NUM, ans, strlen(ans));
                     }
                 }
                 // Reset buffer index
                 index = 0;
+
+                vTaskDelay(250 / portTICK_PERIOD_MS);
+                gpio_set_level(MAIN_PROCESSOR_RESET_PIN, 0);
+                gpio_set_level(STATUS_LED, 0);
             }
             else
             {
@@ -301,7 +345,7 @@ void uart_task(void *pvParameters)
                 else
                 {
                     sprintf(ans, "E0: %s\n", buffer); // E0: buffer overflow
-                    uart_write_bytes(UART_NUM, ans, index + 5);
+                    uart_write_bytes(UART_NUM, ans, strlen(ans));
                     index = 0;
                 }
             }
@@ -314,5 +358,15 @@ void app_main()
     init_uart();
     init_i2c();
 
-    xTaskCreate(uart_task, "uart_task", 4096, NULL, 10, NULL);
+    gpio_set_direction(MAIN_PROCESSOR_RESET_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(STATUS_LED, GPIO_MODE_OUTPUT);
+    gpio_set_level(MAIN_PROCESSOR_RESET_PIN, 0);
+    gpio_set_level(STATUS_LED, 0);
+
+    xTaskCreate(uart_task, "uart_task", 20480, NULL, 10, NULL);
 }
+
+// test pattern
+// PN 1 "00205675" CR 2 "A" PY 1 "08" PW 1 "25" SN 2 "0000000000620089" MI 1 "S" RI 1 "R" MAC 1 "00:10:52:D2:43:FB"
+// PN 1 "00205675" CR 2 "A" PY 1 "08" PW 1 "25" SN 2 "0000000000620456" MI 1 "S" RI 1 "R" MAC 1 "00:10:52:D2:44:FF"
+// 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ9876543210zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA
